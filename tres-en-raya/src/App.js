474847f548986cd8ce4c0210dccb9dbcc2d9a19f
jhosflores <155741749+jhosflores@users.noreply.github.com>
2024-01-17 21:14:25 +0000

    commit

--- tres-en-raya/src/App.js
+++ tres-en-raya/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Tablero from './components/Tablero';
+import TableroB from './components/TableroB';
+import TableroC from './components/TableroC';
+import TableroD from './components/TableroD';
+import TableroE from './components/TableroE';
+import Juego from './components/Juego';
+import JuegoA from './components/JuegoA';
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* <Tablero />
+      <TableroB />
+      <TableroE />
+      <Juego />*/}
+      <JuegoA />
     </div>
   );
 }
